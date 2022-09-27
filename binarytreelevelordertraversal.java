6cc194032eff552bf47f19f97563e401d99b0a07
avraneel <avraneelpal@gmail.com>
2022-09-27 22:48:32 +0530

    Uploading

---
+++ binarytreelevelordertraversal.java
@@ -0,0 +1,39 @@
+// Binary tree level order traversal
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        q.offer(root);
+        if(root == null) return result;
+        while(!q.isEmpty()) {
+            int s = q.size();
+            System.out.println(s);
+            List<Integer> sub = new ArrayList<>();
+            for(int i=0; i<s; i++) {
+                if(q.peek().left != null) 
+                    q.offer(q.peek().left);
+                if(q.peek().right != null)
+                    q.offer(q.peek().right);
+                sub.add(q.poll().val);
+            }
+            result.add(sub);     
+        }
+        return result;
+    }
+}
---
+++ binarytreinordertraversal.java
@@ -0,0 +1,32 @@
+// Inorder Traversal of a binary tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ord = new ArrayList<>();
+        inorder(root, ord);
+        return ord;
+    }
+    
+    public void inorder(TreeNode root, List<Integer> ord) {
+        if(root == null)
+            return;
+        inorder(root.left, ord);
+        ord.add(root.val);
+        inorder(root.right, ord);
+    }
+}
