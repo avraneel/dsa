53b31cac72c165a778e33387862f9890aa45c09e
avraneel <avraneelpal22@gmail.com>
2022-12-29 20:01:08 +0530

    added more hashing problems

---
+++ containsduplicate.java
@@ -0,0 +1,17 @@
+import java.util.*;;
+
+// Check if an array has duplicate elements
+
+class containsduplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(s.contains(nums[i]))
+                return true;
+            else
+                s.add(nums[i]);
+        }
+        return false;
+    }
+}
