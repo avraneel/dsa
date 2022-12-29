1eed11fb0d5160a9d22350ab1b7949cb2d377703
avraneel <avraneelpal22@gmail.com>
2022-12-29 11:38:11 +0530

    added comments to intersection

--- LLintersection.java
+++ LLintersection.java
@@ -1,10 +1,20 @@
 // Link = https://leetcode.com/problems/intersection-of-two-linked-lists/description/
 
+// See diagram in question,
+// (n1 nodes)
+//          \
+//           (c common nodes)   
+//          /
+// (n2 nodes)
+
 public class LLintersection {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode a = headA;
         ListNode b = headB;
         if(headA == null || headB == null) return null;
+
+       // First iteration a will come back to b's head after traversing to entire a's list and b will come to a's head similarly
+       // they will meet in 2nd iteration as a will cover (n1 + c) + n2 and b will cover (n2 + c) + n1 nodes, which are equal. 
         while(a != b) {
             if(a == null) 
                 a = headB;
