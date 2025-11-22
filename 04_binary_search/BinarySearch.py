8fa8710236eb49f77470c1cf1f44d996e7380392
Avraneel Pal <avraneelpal22@gmail.com>
2025-11-22 15:26:42 +0530

    Add binary search

---
+++ 04_binary_search/BinarySearch.py
@@ -0,0 +1,22 @@
+def binary_search(key, nums):
+
+    l = 0
+    r = len(nums)-1
+
+    while l < r:
+        mid = (l + r) // 2
+
+        # if == mid then found
+        if key == nums[mid]:
+            return mid
+
+        #if < mid
+        if key < nums[mid]:
+            r = mid-1
+
+        else:
+            l = mid+1
+
+    return -1
+
+print(binary_search(3, [1,2,3,4,5,6,7,8,9]))
