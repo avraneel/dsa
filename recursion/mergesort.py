5e7fc001f968901096ecad356810d3b53baffe74
Avraneel Pal <avraneelpal22@gmail.com>
2026-03-12 23:30:44 +0530

    Add mergesort

--- recursion/mergesort.py
+++ recursion/mergesort.py
@@ -1,6 +1,7 @@
 def merge(left, right):
     merged = []
-    left_pointer = 0, right_pointer = 0
+    left_pointer = 0
+    right_pointer = 0
     while left_pointer < len(left) and right_pointer < len(right):
         # Loop as long as both pointers point within array
         if left[left_pointer] < right[right_pointer]:
@@ -13,15 +14,16 @@ def merge(left, right):
             right_pointer += 1
     if left_pointer >= len(left):
         # left array exhausted, copy everything remaining from right subarray
-        return merged + left[left_pointer:]
+        return merged + right[right_pointer:]
     else:
         # right array exhausted, copy everything remaining from left subarray
-        return merged + right[right_pointer:]
+        return merged + left[left_pointer:]
+
 
 def mergesort(arr):
     if len(arr) <= 1:
         return arr
     else:
-        left = arr[:len(arr) // 2]
-        right = arr[len(arr) // 2:]
+        left = mergesort(arr[: len(arr) // 2])
+        right = mergesort(arr[len(arr) // 2 :])
         return merge(left, right)
