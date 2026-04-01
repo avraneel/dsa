324947faec30f14ce35288a5d6195d3e7482f9e4
Avraneel Pal <avraneelpal22@gmail.com>
2026-04-01 23:29:31 +0530

    Add JS Hashmap

---
+++ 01_arrays_and_hashing/hashing/HashMap.js
@@ -0,0 +1,30 @@
+class HashMap {
+  constructor(loadFactor, capacity) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.data = [];
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    data[index] = value;
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    if (data[index]) return data[index];
+    else return null;
+  }
+}
