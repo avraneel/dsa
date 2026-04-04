1c93718430ec021454fb90203f33dcbfeac20bdc
Avraneel Pal <avraneelpal22@gmail.com>
2026-04-04 10:23:05 +0530

    Add remaining methods

--- 01_arrays_and_hashing/hashing/HashMap.js
+++ 01_arrays_and_hashing/hashing/HashMap.js
@@ -19,7 +19,7 @@ class HashMap {
 
   set(key, value) {
     const index = this.hash(key);
-    data[index] = value;
+    data[index] = { key: key, value: value };
     if (index / this.capacity > this.loadFactor) {
       this.capacity *= 2;
     }
@@ -27,19 +27,19 @@ class HashMap {
 
   get(key) {
     const index = this.hash(key);
-    if (data[index]) return data[index];
+    if (data[index]) return data[index].value;
     else return null;
   }
 
   has(key) {
     const index = this.hash(key);
-    const val = this.data[index];
+    const val = this.data[index].value;
     return val == undefined ? false : true;
   }
 
   remove(key) {
     const index = this.hash(key);
-    const val = this.data[index];
+    const val = this.data[index].value;
     if (val != undefined) {
       // key found
       this.data[index] = undefined;
@@ -49,11 +49,44 @@ class HashMap {
     }
   }
 
-  length() {}
+  length() {
+    let count = 1;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i] != undefined) {
+        count++;
+      }
+    }
+    return count;
+  }
 
   clear() {
     for (let i = 0; i < this.data.capacity; i++) {
       data[i] = undefined;
     }
+  }
+
+  keys() {
+    const arr = [];
+    this.data.forEach((element) => {
+      arr.push(element.key);
+    });
+    return arr;
+  }
+
+  values() {
+    const arr = [];
+    this.data.forEach((element) => {
+      arr.push(element.value);
+    });
+    return arr;
+  }
+
+  entries() {
+    const arr = [];
+    this.data.forEach((element) => {
+      const item = [element.key, element.value];
+      arr.push(item);
+    });
+    return arr;
   }
 }
