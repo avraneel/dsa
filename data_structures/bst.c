ddbbd47e5f21ccd5244fd50f282fbbbf8bfbaeae
Avraneel Pal <avraneelpal22@gmail.com>
2026-04-26 07:25:47 +0530

    fix formatting

--- data_structures/bst.c
+++ data_structures/bst.c
@@ -1,47 +1,48 @@
 #include <stddef.h>
 
-typedef struct tree {
+typedef struct tree
+{
     int item;
     struct tree *parent;
     struct tree *left;
     struct tree *right;
-    
-} tree; 
 
-tree *search(tree *t, int x) {
-    if(t == NULL || t->item == x)
+} tree;
+
+tree *search(tree *t, int x)
+{
+    if (t == NULL || t->item == x)
         return t;
 
-    if(x < t->item) 
-        return(search(t->left, x));
+    if (x < t->item)
+        return (search(t->left, x));
     else
-        return(search(t->right, x));
+        return (search(t->right, x));
 }
 
-tree *find_minimum(tree *t, int x) {
+tree *find_minimum(tree *t, int x)
+{
     tree *min;
 
-    if(t == NULL)
+    if (t == NULL)
         return NULL;
 
     min = t;
-    while(min->left != NULL) {
+    while (min->left != NULL)
+    {
         min = min->left;
     }
 
     return min;
 }
 
-tree *insert(tree* t, int x) {
+tree *insert(tree *t, int x)
+{
     tree *p = malloc(sizeof(tree));
-
-    
-
 }
 
-
-int main() {
+int main()
+{
 
     return 0;
 }
-
--- data_structures/bst.cpp
+++ data_structures/bst.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 template <typename T>
-struct Node {
+struct Node
+{
 	T key;
-	Node* left;
-	Node* right;
-	Node(T key) {
+	Node *left;
+	Node *right;
+	Node(T key)
+	{
 		this.key = key;
 		left = right = NULL;
 	}
@@ -13,8 +15,10 @@ struct Node {
 
 // inorder tree walk to print out all the keys in a BST in sorted order
 template <typename T>
-void inorderTreeWalk(Node<T>* root) {
-	if(root == NULL) {
+void inorderTreeWalk(Node<T> *root)
+{
+	if (root == NULL)
+	{
 		return;
 	}
 	inorderTreeWalk(root->left);
@@ -22,32 +26,32 @@ void inorderTreeWalk(Node<T>* root) {
 	inorderTreeWalk(root->right);
 }
 
-
 // fn to insert a new node with the given key in BST
 template <typename T>
-Node<T>* insert(Node<T>* node, int key) {
+Node<T> *insert(Node<T> *node, int key)
+{
 
 	// If ke
 }
 
 // function to search a key in BST, recursive method
 template <typename T>
-Node<T>* search(Node<T>* root, T key) {
+Node<T> *search(Node<T> *root, T key)
+{
 
 	// Base cases: root is null or key is present at root
 	// If root is null that means key is not found in tree
-	if(root == NULL || key == root->key)
+	if (root == NULL || key == root->key)
 		return root;
-	
+
 	// Key is lesser than roots key
-	if(key < root->key)
+	if (key < root->key)
 		return search(root->left, key);
-	else 
+	else
 		return search(root->right, key);
 }
 
-
-
-int main() {
+int main()
+{
 	return 0;
 }
