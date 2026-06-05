fb068fd304fb86f9da7ef5fc43339c0cce91f4dd
Avraneel Pal <avraneelpal22@gmail.com>
2026-06-05 06:01:56 +0530

    update formatting

--- 01_arrays_and_hashing/prefixSum/prefixSum.cpp
+++ 01_arrays_and_hashing/prefixSum/prefixSum.cpp
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-#include <bits/stdc++.h>
-using namespace std;
-
-void print(vector<int> &a) {
-    for(auto item: a) {
+void print(vector<int> &a)
+{
+    for (auto item : a)
+    {
         cout << item << " ";
     }
 }
 
 /* Naive approach: pfs[i] = sum(a[i] + a[i-1] + ... + a[0])*/
-vector<int> prefixSumNaive(vector<int> &a) {
+vector<int> prefixSumNaive(vector<int> &a)
+{
     vector<int> pfs(a.size());
 
-    for(int i = 0; i < pfs.size(); i++) {
+    for (int i = 0; i < pfs.size(); i++)
+    {
         int sum = 0;
-        for(int j = 0; j <= i; j++) {
+        for (int j = 0; j <= i; j++)
+        {
             sum += a[j];
         }
         pfs[i] = sum;
@@ -29,22 +30,26 @@ vector<int> prefixSumNaive(vector<int> &a) {
 }
 
 /* Approach 2: prefix[i] = prefix[i-1] + a[i] */
-void prefixSum2(vector<int> &a) {
+void prefixSum2(vector<int> &a)
+{
     vector<int> prefix(a.size());
     prefix[0] = a[0];
-    for(int i = 1; i < a.size(); i++) {
-        prefix[i] = prefix[i-1] + a[i];
+    for (int i = 1; i < a.size(); i++)
+    {
+        prefix[i] = prefix[i - 1] + a[i];
     }
     cout << "\n2nd approach:\n";
     print(prefix);
     cout << "\n";
 }
- 
-void input(vector<int> &a) {
+
+void input(vector<int> &a)
+{
     int s;
     cout << "Enter number of elements: ";
     cin >> s;
-    for(int i = 0; i < s; i++) {
+    for (int i = 0; i < s; i++)
+    {
         int item;
         cout << "Enter element: ";
         cin >> item;
@@ -52,7 +57,8 @@ void input(vector<int> &a) {
     }
 }
 
-int main() {
+int main()
+{
     vector<int> a;
     input(a);
     cout << "\nYour array is\n";
